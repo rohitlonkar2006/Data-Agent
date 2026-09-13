dd3cef1fd331b05202e560dde977218960eb1bba
Rohit Lonkar <lonkarravi00@gmail.com>
2026-09-13 19:50:58 +0530

    done with today. GN😁

---
+++ utils/llm_pick.py
@@ -0,0 +1,12 @@
+def pick_llm(level:str):
+    """
+    Picks the appropriate LLM based on the level of the question
+    
+    Args:
+        level(str) : the level of the question, can be "easy","medium", or "hard".
+    
+    Returns:
+        str: the name of the LLM to be used.
+    """
+    if level.lower == "low":
+            
