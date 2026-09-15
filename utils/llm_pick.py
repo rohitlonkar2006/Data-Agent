7b52cbedd398e40369964333a375325f3391316f
Rohit Lonkar <lonkarravi00@gmail.com>
2026-09-15 20:49:52 +0530

    finish with llm_pick.py

--- utils/llm_pick.py
+++ utils/llm_pick.py
@@ -12,3 +12,15 @@ def pick_llm(level:str):
     """
     if level.lower == "low":
         llm = ChatGroq(model = "llama-3.1-8b-instant", temperature = 0)
+        
+    elif level.lower == "medium":
+        llm = ChatGroq(model = "openai/gtp-oss-120b", temperature = 0)    
+    elif level.lower == "high":
+        llm = ChatGroq(model = "openai/gpt-oss-120b", temperature = 0)
+    else:
+        raise ValueError(f"Unsupported level: {level}")
+    
+    return llm
+
+llm_obj = pick_llm("low")
+llm_obj.invoke("Who is modi")
