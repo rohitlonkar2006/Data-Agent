fdd1d2fa6d102c5268798170bef73a82a6e36e96
Rohit Lonkar <lonkarravi00@gmail.com>
2026-09-18 15:04:57 +0530

    taking short break

--- utils/database.py
+++ utils/database.py
@@ -1,1 +1,14 @@
 import psycopg2
+
+class DatabaseUtil:
+    def __init__(self, db_config):
+        self.db_config=db_config
+        
+        try:
+            self.connection = psycopg2.connect(**db_config)
+            
+        except Exception as e:
+            print(f"Error connecting to the database: {e}")
+            self.connection = None
+    def schema_details(self, schema_name):
+        
