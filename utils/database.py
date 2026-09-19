52e5ad4a35044c9754acb353f413f14dc093abf7
Rohit Lonkar <lonkarravi00@gmail.com>
2026-09-19 11:56:01 +0530

    updated database.py

--- utils/database.py
+++ utils/database.py
@@ -12,3 +12,27 @@ class DatabaseUtil:
             self.connection = None
     def schema_details(self, schema_name):
         
+        schema_info_context = ""
+        
+        connection = self.connection
+        cursor = connection.cursor()
+        
+        schema_info_context = f"Database Schema: {schema_name}\n"
+        
+        cursor.execute("SELECT table_name FROM information_schema.tables WHERE table_schema = %s;", (schema_name))
+        tables_list = cursor.fetchall()
+        
+        for table in tables_list:
+            table_name = table[0]
+            schema_info_context = f"{schema_info_context}\nTable: {table_name}\n"
+            
+            cursor.execute("SELECT column_name, data_type FROM information_schema.columns WHERE table_name = %s;",(table_name))
+            columns_list = cursor.fetchall()
+
+            for column in columns_list:
+                column_name = column[0]
+                data_type = column[1]
+                schema_info_context = f"{schema_info_context} Column: {column_name}, Data Type: {data_type}\n"
+                
+        return schema_info_context
+    
